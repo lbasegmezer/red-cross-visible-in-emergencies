2562a01ccabb4d540efca83b5363b7f7973e5de9
Harry Trimble <harryroberttrimble@gmail.com>
2019-10-20 22:27:27 +0100

    Add stepper counter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,6 +37,25 @@ function onClick() {
     document.getElementById("clicks").innerHTML = clicks;
 };
 
+/* stepper */
+
+$(document).on('click', '.number-spinner button', function () {    
+  var btn = $(this),
+    oldValue = btn.closest('.number-spinner').find('input').val().trim(),
+    newVal = 0;
+  
+  if (btn.attr('data-dir') == 'up') {
+    newVal = parseInt(oldValue) + 1;
+  } else {
+    if (oldValue > 1) {
+      newVal = parseInt(oldValue) - 1;
+    } else {
+      newVal = 1;
+    }
+  }
+  btn.closest('.number-spinner').find('input').val(newVal);
+});
+
 // autocomplete
 var names = ["Laura Basegmezer","Jo Straw","Harry Trimble","Christina Pekkola Tellez","Laura Turner","Rosie Slater-Carr","Ian Goodman","Ian Hopper"];
 
@@ -138,4 +157,3 @@ document.addEventListener("click", function (e) {
 }
 
 autocomplete(document.getElementById("myInput"), names);
-
